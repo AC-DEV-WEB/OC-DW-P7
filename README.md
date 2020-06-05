46ed077b5056c24fdeace70e194cc1f997d9892b
AC-DEV-WEB <ac.dev.webapp@gmail.com>
2020-06-05 18:00:58 +0200

    Automatisation de l'installation
    Configuration du package "concurrently", automatisation de l'installation du projet et de sa mise en route.
    Mise à jour du README.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # OpenClassrooms - Développeur Web - Projet n°7
 Créez un réseau social d’entreprise
+
+## Installation
+
+Installer `npm install`.
+Démarrer `npm run dev` pour avoir accès au serveur de développement. 
+Rendez-vous sur `http://localhost:4200/`. 
+L'application va se recharger automatiquement si vous modifiez un fichier source.
--- back-end/app.js
+++ back-end/app.js
@@ -33,7 +33,7 @@ db.sequelize.sync();
 
 // middleware qui permet l'accès à toutes les origines d'accéder à l'API (CORS) 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:4200');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
